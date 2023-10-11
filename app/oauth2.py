5fe7fedb262236a8fef553b896559f3880b47d2e
Raymond <raymond.w.lang@gmail.com>
2023-10-10 19:57:33 -0700

    added testing for posts

--- app/oauth2.py
+++ app/oauth2.py
@@ -36,7 +36,7 @@ def verify_access_token(token: str, credentials_exception):
         id: str = payload.get("user_id")
         if id is None:
             raise credentials_exception
-        token_data = schemas.TokenData(id=id)
+        token_data = schemas.TokenData(id=str(id))
     except JWTError:
         raise credentials_exception
 
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,20 +1,9 @@
 import pytest
 from jose import jwt
 from app import schemas
-from .database import client, session
 from app.config import settings
 
-@pytest.fixture
-def test_user(client):
-    user_data = {"email": "sanjeev@gmail.com",
-                 "password": "password123"}
-    res = client.post("/users/", json=user_data)
 
-    assert res.status_code == 201
-
-    new_user = res.json()
-    new_user['password'] = user_data['password']
-    return new_user
 
 def test_root(client):
     res = client.get("/")
@@ -29,3 +18,17 @@ def test_login_user(test_user, client):
     assert id == test_user['id']
     assert login_res.token_type == "bearer"
     assert res.status_code == 200
+
+@pytest.mark.parametrize("email, password, status_code", [
+    ('wrongemail@gmail.com', 'password123', 403),
+    ('raymond@gmail.com', 'wrongpassword', 403),
+    ('wrongemail@gmail.com', 'wrongpassword', 403),
+    (None, 'password123', 422),
+    ('raymond@gmail.com', None, 422)
+])
+def test_incorrect_login(test_user, client, email, password, status_code):
+    res = client.post(
+        "/login", data={"username": email, "password": password})
+
+    assert res.status_code == status_code
+    # assert res.json().get('detail') == 'Invalid Credentials'
