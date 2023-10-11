1f0f384f2099a7fffe05434851917e216620edaf
Raymond <raymond.w.lang@gmail.com>
2023-10-10 17:18:21 -0700

    added testing for login user and create user

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,8 +1,30 @@
-from fastapi.testclient import TestClient
-from app.main import app
+import pytest
+from app import schemas
+from .database import client, session
 
-client = TestClient(app)
+@pytest.fixture
+def test_user(client):
+    user_data = {"email": "sanjeev@gmail.com",
+                 "password": "password123"}
+    res = client.post("/users/", json=user_data)
 
-def test_root():
+    assert res.status_code == 201
+
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+    return new_user
+
+def test_root(client):
     res = client.get("/")
     print (res)
+
+def test_login_user(test_user, client):
+    res = client.post(
+        "/login", data={"username": test_user['email'], "password": test_user['password']})
+    assert res.status_code == 200
+
+def test_login_user(test_user, client):
+    res = client.post(
+        "/login", data = {"email": test_user['email'], "password": test_user['password']}
+    )
+    assert res.status_code == 200
